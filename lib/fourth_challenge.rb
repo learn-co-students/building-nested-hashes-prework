9a510c01fbedd92e0d21f8a33ecde84cb42118ee
cmcorrales <cmcorrales3@gmail.com>
2018-06-19 09:26:05 -0400

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,8 @@ def fourth_challenge
     },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
+        :status => "alive"
     },
       :hero_friends => [
         {
